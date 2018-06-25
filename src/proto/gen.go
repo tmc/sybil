89babcf8aadb2e5df1da8fbeb450f50d99480967
Travis Cline <travis.cline@gmail.com>
2018-06-25 15:56:30 -0700

    [protos] move FlagDefs to proto

--- src/proto/gen.go
+++ src/proto/gen.go
@@ -1,3 +1,4 @@
 //go:generate protoc -I . -I ../../../../.. sybil.proto --go_out=plugins=grpc:../sybilpb/
+//go:generate protoc -I . -I ../../../../.. internal.proto --gogofast_out=../internal/internalpb
 
 package proto
