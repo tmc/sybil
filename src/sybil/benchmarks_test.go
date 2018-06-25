d65ae3445aac760930416391a2ead16dd94403e1
Travis Cline <travis.cline@gmail.com>
2018-06-25 14:03:26 -0700

    [benchmarks] add basic SaveRecordsToColumns benchmarks

--- src/sybil/benchmarks_test.go
+++ src/sybil/benchmarks_test.go
@@ -49,3 +49,46 @@ func BenchmarkAddRecordsB(b *testing.B) {
 		r.AddStrField("age_str", strconv.FormatInt(int64(age), 10))
 	}
 }
+
+func BenchmarkSaveRecordsToColumnsA(b *testing.B) {
+	tableName := getTestTableName(nil)
+	deleteTestDb(tableName)
+	defer deleteTestDb(tableName)
+	defer b.StopTimer()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		tbl := GetTable(tableName)
+		r := tbl.NewRecord()
+		r.AddIntField("id", int64(i))
+		b.StartTimer()
+		tbl.SaveRecordsToColumns()
+		b.StopTimer()
+		unloadTestTable(tableName)
+	}
+}
+
+func BenchmarkSaveRecordsToColumnsB(b *testing.B) {
+	tableName := getTestTableName(nil)
+	deleteTestDb(tableName)
+	defer deleteTestDb(tableName)
+	defer b.StopTimer()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		tbl := GetTable(tableName)
+		r := tbl.NewRecord()
+		r.AddIntField("id", int64(i))
+		age := int64(rand.Intn(20)) + 10
+		r.AddIntField("age", age)
+		r.AddStrField("age_str", strconv.FormatInt(int64(age), 10))
+
+		b.StartTimer()
+		tbl.SaveRecordsToColumns()
+		b.StopTimer()
+
+		unloadTestTable(tableName)
+	}
+}
